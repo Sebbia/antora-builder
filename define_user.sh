75a58b13bb854a9b090000cb5c98950dad33b95a
Vladislav Navrocky <navrocky.vlad@gmail.com>
2019-12-23 16:57:11 +0300

    Fix define user script inside docker image. Version 1.2.1

--- define_user.sh
+++ define_user.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 USER_NAME=builder
 GROUP_NAME=builder
 
@@ -9,11 +12,19 @@ GROUP_ID=$2
 if grep -q ":x:${GROUP_ID}:" /etc/group; then
     sed -r -i "s/\w+:x:${GROUP_ID}/${GROUP_NAME}:x:${GROUP_ID}/" /etc/group
 else
-    groupadd -g ${GROUP_ID} ${GROUP_NAME}
+    if which groupadd >/dev/null; then
+        groupadd -g ${GROUP_ID} ${GROUP_NAME}
+    else
+        addgroup -g ${GROUP_ID} ${GROUP_NAME}
+    fi
 fi
 
 if grep -q ":x:${USER_ID}:" /etc/passwd; then
     sed -r -i "s/\w+:x:${USER_ID}:[0-9]+:/${USER_NAME}:x:${USER_ID}:${GROUP_ID}:/" /etc/passwd
 else
-    useradd -u ${USER_ID} -g ${GROUP_ID} ${USER_NAME}
+    if which useradd >/dev/null; then
+        useradd -u ${USER_ID} -g ${GROUP_ID} ${USER_NAME}
+    else
+        adduser -D -u ${USER_ID} -G ${GROUP_NAME} ${USER_NAME}
+    fi
 fi
